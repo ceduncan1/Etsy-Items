42c01ba386ff31f6fb619337b9b4ef1bb571b665
Cary Duncan <duncan.cary@gmail.com>
2015-10-01 14:21:07 -0400

    Added JS folder and plan for problems

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// 1. map price, find sum, divide by array length (number of prices)
+
+
+
+
+
+// 2. map item.price, if 14 < price < 18
+
+
+
+
+
+// 3. if currency code = GBP, return (name) + 'costs' + (price)
+
+
+
+
+
+// 4. map materials 'wood'
+
+
+
+
+
+// 5. if materials.length >= 8, return name, materials.length, and materials
+
+
+
+
+
+// 6. filter who_made, i_did into an array; return 
+
+
+
+
